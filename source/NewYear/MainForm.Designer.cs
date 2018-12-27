bc829e5871a2ba83694421bc94012bef8c87898c
MagicFun1241 <pig-l@mail.ru>
2018-12-27 08:38:58 +0400

    Ready for release
    Happy new Year :)

---
+++ source/NewYear/MainForm.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace NewYear
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timerYear = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "До нового года осталось:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(26, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 16);
+            this.label2.TabIndex = 1;
+            // 
+            // timerYear
+            // 
+            this.timerYear.Enabled = true;
+            this.timerYear.Interval = 1000;
+            this.timerYear.Tick += new System.EventHandler(this.timerYear_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(404, 277);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Создано MFSoftware";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(541, 301);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Отсчет времени до нового года";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timerYear;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ source/NewYear/MainForm.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NewYear
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        public int days;
+        public int h;
+        public int m;
+        public int s;
+
+        public string GetDeclension(int number, string nominativ, string genetiv, string plural)
+        {
+            number = number % 100;
+            if (number >= 11 && number <= 19)
+                return plural;
+
+            var i = number % 10;
+            switch (i)
+            {
+                case 1:
+                    return nominativ;
+                case 2:
+                case 3:
+                case 4:
+                    return genetiv;
+                default:
+                    return plural;
+            }
+        }
+
+        private void timerYear_Tick(object sender, EventArgs e)
+        {
+            DateTime NewYear = new DateTime(DateTime.Now.Year, 12, 31);
+            DateTime Now = DateTime.Now;
+
+            TimeSpan ts = NewYear - Now;
+            days = (int)ts.TotalDays;
+            h = (int)ts.TotalHours;
+            m = (int)ts.TotalMinutes;
+            s = (int)ts.Seconds;
+
+            while (h > 24)
+            {
+                h = h - 24;
+                days = days + 1;
+            }
+
+            while (m > 60)
+            {
+                m = m - 60;
+                h = h + 1;
+            }
+
+            string ss = GetDeclension(s, "секунда", "секунды", "секунд");
+            string ms = GetDeclension(m, "минута", "минуты", "минут");
+            string hs = GetDeclension(h, "час", "часа", "часов");
+            string dayss = GetDeclension(days, "день", "дня", "дней");
+
+            if (days == 0)
+            {
+                if (h < 2)
+                {
+                    label2.Text = string.Format("{0} {1}, {2} {3}", h, hs, m, ms);
+                    if (h <= 0)
+                    {
+                        label2.Text = string.Format("{0} {1}, {2} {3}", m, ms, s, ss);
+                        if (m <= 0)
+                        {
+                            label2.Text = string.Format("{0} {1}", s, ss);
+                            if (s <= 0)
+                                label2.Text = "С новым годом";
+                        }
+                    }
+                }
+                else
+                {
+                    label2.Text = string.Format("{0} {1}, {2} {3}", h, hs, m, ms);
+                }
+            }
+            else if (days == 1)
+            {
+                label2.Text = string.Format("{0} {1}, {2} {3}, {4} {5}", days, dayss, h, hs, m, ms);
+            }
+            else if (days > 1)
+            {
+                label2.Text = string.Format("{0} {1}, {2} {3}, {4} {5}", days, dayss, h, hs, m, ms);
+            }
+        }
+    }
+}
